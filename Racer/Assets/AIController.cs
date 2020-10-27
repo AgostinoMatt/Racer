d9980bd8951f427923532f0c94fde3e8243cd62c
AgostinoMatt <AgostinoMatt@users.noreply.github.com>
2020-10-26 22:01:58 -0400

    map
    map waypoints ai controller

--- Racer/Assets/AIController.cs
+++ Racer/Assets/AIController.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class AIController : MonoBehaviour
 {
+    public Circuit circuit;
+    Drive ds;
+    public float steeringSensitivity = 0.01f;
+    Vector3 target;
+    int currentWP = 0;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ds = this.GetComponent<Drive>();
+        target = circuit.waypoints[currentWP].transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 localTarget = ds.rb.gameObject.transform.InverseTransformPoint(target);
+        float distanceToTarget = Vector3.Distance(target, ds.rb.gameObject.transform.position);
+
+        float targetAngle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
+
+        float steer = Mathf.Clamp(targetAngle * steeringSensitivity, -1, 1) * Mathf.Sign(ds.currentSpeed);
+        float accel = 1f;
+        float brake = 0;
+
+        /*if (distanceToTarget < 10)
+        {
+            brake = 0.5f;
+            accel = 0.1f;
+        }*/
+
+        ds.Go(accel, steer, brake);
+
+        if (distanceToTarget < 12) // adjust for closeness to waypoints
+        {
+            currentWP++;
+            if (currentWP >= circuit.waypoints.Length)
+                currentWP = 0;
+            target = circuit.waypoints[currentWP].transform.position;
+        }
+
+        ds.CheckForSkid();
+        ds.CalculateEngineSound();
     }
 }
---
+++ Racer/Assets/antirollBar.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class antirollBar : MonoBehaviour
+{
+    public float antiRoll = 5000.0f;
+    public WheelCollider wheelLFront;
+    public WheelCollider wheelRFront;
+    public WheelCollider wheelLBack;
+    public WheelCollider wheelRBack;
+    public GameObject COM;
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+        //rb.centerOfMass = COM.transform.localPosition;
+    }
+
+    void GroundWheels(WheelCollider WL, WheelCollider WR)
+    {
+        WheelHit hit;
+        float travleL = 1.0f;
+        float travleR = 1.0f;
+
+        bool groundedL = WL.GetGroundHit(out hit);
+        if (groundedL)
+            travleL = (-WL.transform.InverseTransformPoint(hit.point).y - WL.radius) / WL.suspensionDistance;
+
+        bool groundedR = WL.GetGroundHit(out hit);
+        if (groundedR)
+            travleR = (-WR.transform.InverseTransformPoint(hit.point).y - WR.radius) / WR.suspensionDistance;
+
+        float antiRollForce = (travleL - travleR) * antiRoll;
+
+        if (groundedL)
+            rb.AddForceAtPosition(WL.transform.up * -antiRollForce, WL.transform.position);
+
+        if (groundedR)
+            rb.AddForceAtPosition(WR.transform.up * antiRollForce, WR.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GroundWheels(wheelLFront, wheelRFront);
+        GroundWheels(wheelLBack, wheelRBack);
+    }
+}
