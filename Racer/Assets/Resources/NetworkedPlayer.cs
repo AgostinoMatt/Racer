824434ced89ddacf231b7e1033688d8f0956957b
alexxquan <41336089+alexxquan@users.noreply.github.com>
2020-11-03 14:00:35 -0500

    multiplayer scene added
    scene added and adjustments to prefabs to create new ones to be put in the resources folder

---
+++ Racer/Assets/Resources/NetworkedPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Photon.Realtime;
+using Photon.Pun;
+
+public class NetworkedPlayer : MonoBehaviourPunCallbacks
+{
+    public static GameObject LocalPlayerInstance;
+    public GameObject playerNamePrefab;
+    public Rigidbody rb;
+    public Renderer jeepMesh;
+}
